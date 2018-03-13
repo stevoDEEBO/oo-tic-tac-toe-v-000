60daa659b7fd9f78c5827135887ef849317ea7e8
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-03-13 04:10:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,7 +25,13 @@ class TicTacToe
     user_input.to_i - 1
   end
 
-
+  def move(board, index, current_player)
+    board[index] = current_player
+  end
+  
+  def position_taken?(board, location)
+    board[location] != " " && board[location] != ""
+  end
 
 
 
