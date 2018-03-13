497225da5078962b33a13b910586625a18b4fdf3
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-03-13 04:18:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,21 @@ class TicTacToe
     @board[location] == "X" || @board[location] == "O"
   end
 
-
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
 
 
 
